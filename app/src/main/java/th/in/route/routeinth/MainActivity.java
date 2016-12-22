cc88ab12be06ed89e788d61bdef4a3164e5ef3eb
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-12-22 14:59:24 +0700

    clean up

--- app/src/main/java/th/in/route/routeinth/MainActivity.java
+++ app/src/main/java/th/in/route/routeinth/MainActivity.java
@@ -1,15 +1,13 @@
 package th.in.route.routeinth;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import th.in.route.routeinth.model.result.Result;
 
-public class MainActivity extends AppCompatActivity implements MainFragment.OnCalculateBtnPressedListener, ResultFragment.OnTest {
+public class MainActivity extends AppCompatActivity {
     @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
@@ -18,24 +16,10 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnCa
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        MainFragment mainFragmentFragment = MainFragment.newInstance();
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.flContent, mainFragmentFragment).commit();
-    }
-
-    @Override
-    public void OnCalculateBtnPressed(Result result) {
-        ResultFragment resultFragment = ResultFragment.newInstance();
-        resultFragment.setResult(result);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.flContent, resultFragment).addToBackStack(null).commit();
-    }
-
-    @Override
-    public void onTest() {
-
+//
+//        MainFragment mainFragmentFragment = MainFragment.newInstance();
+//
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction().replace(R.id.flContent, mainFragmentFragment).commit();
     }
 }
