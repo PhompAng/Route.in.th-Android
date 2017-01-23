ba589918aac9d16d672d856e57becd288a3a01a3
tharamalai <57070054@kmitl.ac.th>
2017-01-23 19:51:52 +0700

    persistent bottom sheet

--- app/src/main/java/th/in/route/routeinth/StationMapActivity.java
+++ app/src/main/java/th/in/route/routeinth/StationMapActivity.java
@@ -2,8 +2,17 @@ package th.in.route.routeinth;
 
 import android.content.Intent;
 import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +23,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import th.in.route.routeinth.app.DpiUtils;
 import th.in.route.routeinth.model.system.Station;
 
 public class StationMapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -21,11 +35,14 @@ public class StationMapActivity extends AppCompatActivity implements OnMapReadyC
     private GoogleMap mMap;
     private Location stationLocation;
     private Station station;
+    Unbinder unbinder;
+    BottomSheetBehavior behavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_map);
+        unbinder = ButterKnife.bind(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -34,7 +51,6 @@ public class StationMapActivity extends AppCompatActivity implements OnMapReadyC
         Intent intent = getIntent();
         stationLocation = intent.getParcelableExtra("location");
         station = Parcels.unwrap(intent.getParcelableExtra("station"));
-
 
     }
 
@@ -45,6 +61,14 @@ public class StationMapActivity extends AppCompatActivity implements OnMapReadyC
         // Add a marker in Sydney, Australia, and move the camera.
         mMap.moveCamera(CameraUpdateFactory.newLatLng(stationLatLong));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
+
+    }
+
+    public void showStationDetail(){
+//        View v = LayoutInflater.from(StationMapActivity.this).inflate(R.layout.bottom_station_detail, null);
+        View mBottomSheetLayout = findViewById(R.id.bottom_statin_detail);
+        behavior = BottomSheetBehavior.from(mBottomSheetLayout);
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
     }
 }
