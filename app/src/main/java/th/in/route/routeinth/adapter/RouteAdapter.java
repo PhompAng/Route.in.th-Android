4b4304c40e2279444c02fea853b6abed8b9d63a8
Pichai Sivawat <tingtong003tomy@gmail.com>
2017-01-31 01:46:00 +0700

    show facilities icon

--- app/src/main/java/th/in/route/routeinth/adapter/RouteAdapter.java
+++ app/src/main/java/th/in/route/routeinth/adapter/RouteAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.GradientDrawable;
-import android.media.Image;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
@@ -13,8 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import org.parceler.Parcels;
 
@@ -27,8 +32,6 @@ import th.in.route.routeinth.app.DistanceUtils;
 import th.in.route.routeinth.app.StationUtils;
 import th.in.route.routeinth.model.result.Route;
 import th.in.route.routeinth.model.view.RouteItem;
-
-import static java.security.AccessController.getContext;
 
 /**
  * Created by Acer on 20/12/2559.
@@ -47,6 +50,25 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> 
         this.mContext = mContext;
         this.fragment = resultFragment;
         isNavigate = false;
+    }
+
+    private void hasFacilities(final ImageView holder, String key) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child("facilities").child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<String>> indicator = new GenericTypeIndicator<List<String>>(){};
+                List<String> facilities = dataSnapshot.getValue(indicator);
+                if (facilities.contains("elevator") || facilities.contains("ramp")) {
+                    holder.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void setNearestKey(String nearestKey) {
@@ -90,11 +112,11 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> 
             @Override
             public void onClick(View v) {
                 Log.d("type", routeItem.getType());
-                if(routeItem.getType().equals("station")){
+                if (routeItem.getType().equals("station")) {
                     fragment.setIsShow(routeItem.getSystem(), false);
-                }else if(routeItem.getType().equals("between")){
+                } else if(routeItem.getType().equals("between")) {
                     fragment.setIsShow(routeItem.getSystem(), true);
-                }else {
+                } else {
                     Intent intent = new Intent(mContext, StationMapActivity.class);
                     intent.putExtra("station", Parcels.wrap(StationUtils.getInstance().getStationFromKey(routeItem.getRoute().name.key)));
                     intent.putExtra("location", DistanceUtils.getInstance().getLocationFromKey(routeItem.getRoute().name.key));
@@ -137,6 +159,7 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> 
 
 //        GradientDrawable viewAllStationBg = (GradientDrawable) holder.viewAllStationLabel.getBackground();
 
+        holder.infoLabel.setVisibility(View.GONE);
         //set code label and heading label
         if (!routeItem.getType().equals("between")) {
             holder.viewCodeLabel.setText(routeItem.getRoute().name.code);
@@ -145,11 +168,10 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> 
             holder.viewCodeLabel.setVisibility(View.VISIBLE);
             holder.viewHeadingLabel.setText("Heading:  " + routeItem.getRoute().heading.en);
             holder.viewHeadingLabel.setVisibility(View.VISIBLE);
-            holder.infoLabel.setVisibility(View.VISIBLE);
+            hasFacilities(holder.infoLabel, routeItem.getRoute().name.key);
         } else {
             holder.viewCodeLabel.setVisibility(View.GONE);
             holder.viewHeadingLabel.setVisibility(View.GONE);
-            holder.infoLabel.setVisibility(View.GONE);
         }
 
         //each listview
