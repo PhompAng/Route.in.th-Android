10f5eefe0601cc74d11fe8613314a9754dcdf3fd
tharamalai <57070054@kmitl.ac.th>
2017-02-01 19:54:49 +0700

    add  disclaimer

--- app/src/main/java/th/in/route/routeinth/SettingFragment.java
+++ app/src/main/java/th/in/route/routeinth/SettingFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
 
 import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
 
@@ -21,6 +22,16 @@ public class SettingFragment extends PreferenceFragmentCompat implements SharedP
     @Override
     public void onCreatePreferencesFix(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference, rootKey);
+
+        Preference preference_disclaimer = (Preference) findPreference("preference_disclaimer");
+        preference_disclaimer.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getContext(), DisclaimerActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
     @Override
